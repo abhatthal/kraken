e923bbe6ca24b3984b1f9359b8d698bd434446c7
Akash Bhatthal <akashbhatthal@gmail.com>
2019-08-08 02:31:42 -0700

    Added 8ball command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import discord
+import random
 from discord.ext import commands
 from os import environ
 
@@ -24,7 +25,33 @@ async def on_member_remove(member):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('Pong!')
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = [
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a doubt.',
+        'Yes - definitely.',
+        'You may rely on it.',
+        'As I see it, yes.',
+        'Most likely.',
+        'Outlook good.',
+        'Yes.',
+        'Signs point to yes.',
+        'Reply hazy, try again.',
+        'Ask again later.',
+        'Better not tell you now.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        "Don't count on it.",
+        'My reply is no.',
+        'My sources say no.',
+        'Outlook not so good.',
+        'Very doubtful.'
+    ]
+    await ctx.send(f'Question {question}\nAnswer: {random.choice(responses)}')
 
 bot.run(environ['token'])
 
