f2e333f93fe430eb3256ec66788bbac6ab6707e0
Akash Bhatthal <akashbhatthal@gmail.com>
2019-08-11 15:40:22 -0700

    import ffmpeg, voice channel check

--- cogs/audio.py
+++ cogs/audio.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+from discord.utils import get
 
 class Audio(commands.Cog):
 
@@ -8,7 +10,10 @@ class Audio(commands.Cog):
 
     @commands.command(pass_context=True)
     async def join(self, ctx):
-        channel = ctx.message.author.voice.voice_channel
+        channel = ctx.message.author.voice.channel
+        if not channel:
+            await ctx.send("You are not connected to a voice channel")
+            return
         await self.bot.join_voice_channel(channel)
 
     @commands.command(pass_context=True)
