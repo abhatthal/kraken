0e33b241f14176785088bdc2ba89fed6e2ff2c9a
Akash Bhatthal <akashbhatthal@gmail.com>
2019-08-12 01:46:00 -0700

    First attempt at play command

--- cogs/audio.py
+++ cogs/audio.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 from discord.utils import get
+import youtube_dl
+import os
 
 class Audio(commands.Cog):
 
@@ -35,6 +37,44 @@ class Audio(commands.Cog):
         else:
             await ctx.send('[ERROR] Bot is not in a voice channel')
 
+    @commands.command(pass_context=True)
+    async def play(self, ctx, url : str):
+        song_there = os.path.isfile('song.mp3')
+        # try delete because if song is in use, will throw permissions error
+        try:
+            if song_there:
+                os.remove('song.mp3')
+        except PermissionError:
+            await ctx.send('[ERROR] Music playing')
+            return
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+        # options for download
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192'
+            }]
+        }
+        # download song
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        # rename downloaded song into song.mp3
+        for file in os.listdir('./'):
+            if file.endswith('.mp3'):
+                name = file
+                os.rename(file, 'song.mp3')
+                break
+        # play the song
+        voice.play(FFmpegPCMAudio('song.mp3'))
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+        # Note: don't go above 0.5, very loud. 0.07 is good
+        voice.source.volume = 0.07
+        nname = name.rsplit('-', 2)
+        await ctx.send(f'[PLAY] {nname}')
+        
+        
         
 def setup(bot):
     bot.add_cog(Audio(bot))
