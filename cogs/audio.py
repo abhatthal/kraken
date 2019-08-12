c279427314afb8d1a154fea0082d2d25e5e7eedd
Akash Bhatthal <akashbhatthal@gmail.com>
2019-08-11 23:35:11 -0700

    now using rewrite commands

--- cogs/audio.py
+++ cogs/audio.py
@@ -10,17 +10,25 @@ class Audio(commands.Cog):
 
     @commands.command(pass_context=True)
     async def join(self, ctx):
+        global voice
         channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
         if not channel:
             await ctx.send("You are not connected to a voice channel")
+        # if bot already in a voice channel, move to the vc of author
+        else if voice and voice.is_connected():
+            await voice.move_to(channel)
         else:
-            await self.bot.join_voice_channel(channel)
+            voice = await channel.connect()
 
     @commands.command(pass_context=True)
     async def leave(self, ctx):
-        server = ctx.message.server
-        voice_client = self.bot.voice_client_in(server)
-        await voice_client.disconnect()
+        global voice
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+        if voice:
+            voice.disconnect()
+        else:
+            await ctx.send("Bot is not in a voice channel")
         
 def setup(bot):
     bot.add_cog(Audio(bot))
