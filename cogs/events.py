6515ec9c916880b63fd338488e8b6f2b48953915
Akash Bhatthal <akashbhatthal@gmail.com>
2019-08-14 17:36:40 -0700

    changed from oh to so in creeper event

--- cogs/events.py
+++ cogs/events.py
@@ -27,7 +27,7 @@ class Events(commands.Cog):
         # If a message is sent not in #debate and is not sent by the bot
         if message.channel.id != 606880223199363172 and self.bot.user.id != message.author.id:
             if 'aww man' in message.content.lower() or 'aw man' in message.content.lower():
-                await message.channel.send('Oh we back in the mine')
+                await message.channel.send('So we back in the mine')
             elif 'creeper' in message.content.lower():
                 await message.channel.send('aww man')
 
