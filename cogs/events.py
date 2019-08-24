1debb599faf658d426f0b4858abfb98df93d2a7a
Akash Bhatthal <akashbhatthal@gmail.com>
2019-08-23 23:43:28 -0700

    fixed temp_ban on startup

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 import time
 from time import gmtime, strftime
 from random import choice
@@ -45,28 +46,41 @@ class Events(commands.Cog):
             ''')            
         db.commit()
 
-        # On startup, continue tempbans (in case of server outage)
-        # channel = self.bot.get_channel(settings.LOGGING_CHANNEL)
-        # cursor.execute('SELECT member_id, unban_time FROM tempbans ORDER BY unban_time ASC')
-        # all_rows = cursor.fetchall()
-        # for row in all_rows:
-        #     some_member = discord.Object(id = int(row[0]))
-        #     time_to_wait = int(row[1]) - time.time()
-        #     if time_to_wait < 0:
-        #         time_to_wait = 0
-        #     await asyncio.sleep(time_to_wait)
-        #     logger.info(f'[UNBAN] <@{row[0]}>\n Moderator: {settings.BOT_NAME}')
-        #     eObj = discord.Embed(title = '', type = 'rich')
-        #     eObj.set_author(name = f'[UNBAN] <@{row[0]}>')
-        #     eObj.colour = 0x05A000
-        #     if eObj is not False:
-        #         await channel.send(embed = eObj)
-        #         await discord.Guild.unban(some_member)
-        #         cursor.execute(f'DELETE FROM tempbans WHERE member_id = {row[0]}')
-        #         db.commit()
-
         logger.info('Bot Online')
         print('Bot Online')
+
+        # On startup, continue tempbans (in case of server outage)
+        channel = self.bot.get_channel(settings.LOGGING_CHANNEL)
+        main_guild = self.bot.get_guild(settings.MAIN_GUILD)
+        # support_guild = self.bot.get_guild(settings.SUPPORT_GUILD)
+
+        banned_users = await main_guild.bans()
+        cursor.execute('SELECT member_id, unban_time FROM tempbans ORDER BY unban_time ASC')
+        all_rows = cursor.fetchall()
+
+        for row in all_rows:
+            # get banned member
+            for ban_entry in banned_users:
+                user = ban_entry.user
+                if user.id == row[0]:
+                    break
+            time_to_wait = int(row[1]) - time.time()
+            if time_to_wait < 0:
+                time_to_wait = 0
+            await asyncio.sleep(time_to_wait)
+            eObj = discord.Embed(title = '', type = 'rich')
+            eObj.set_author(name = f'[UNBAN] {user}')
+            eObj.colour = 0x05A000
+            if eObj is not False:
+                # in the event that the user was manually unbanned, just remove from the tempban table
+                try:
+                    logger.info(f'[UNBAN] {user}\n Moderator: {settings.BOT_NAME}')
+                    await channel.send(embed = eObj)
+                    await main_guild.unban(user)
+                except:
+                    pass
+                cursor.execute(f'DELETE FROM tempbans WHERE member_id = {row[0]}')
+                db.commit()
 
 
     @commands.Cog.listener()
@@ -91,7 +105,7 @@ class Events(commands.Cog):
                 await message.channel.send('NO U')
 
             if 'uwu' in message.content.lower():
-                responses = ['urusai kono weebu yarou! >.<', 'damare!', 'kono baka', 'baka', 'uwu dattebayo']
+                responses = ['urusai!', 'baka', 'uwu dattebayo']
                 await message.channel.send(choice(responses))
         
             # allow people to vote by in certain channels
