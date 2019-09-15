22f08d0c18121b6780a7091297df772126463849
Akash Bhatthal <abhattha@sfu.ca>
2019-09-15 11:43:26 -0700

    updated joke description

--- cogs/member.py
+++ cogs/member.py
@@ -239,13 +239,13 @@ class Member(commands.Cog):
             await ctx.send(embed = eObj)
 
 
-    @commands.command(description = "Get a random joke")
+    @commands.command(description = "Get a random dad joke")
     async def joke(self, ctx):
         headers = {'Accept':'application/json'}
         async with aiohttp.ClientSession(headers = headers) as cs:
             async with cs.get('https://icanhazdadjoke.com') as r:
                 res = await r.json()
-                
+
         eObj = await embed(ctx, title = 'Joke', author = settings.BOT_NAME, avatar = settings.BOT_AVATAR,
         description = res['joke'], footer = 'Ha!')
         if eObj is not False:
