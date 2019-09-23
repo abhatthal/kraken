c19d4e0e5a862dd8c6fbed89c603400be9e1b068
Akash Bhatthal <akashbhatthal@gmail.com>
2019-09-23 00:30:12 -0700

    unloaded music, added music channel to link exceptions

--- cogs/events.py
+++ cogs/events.py
@@ -147,7 +147,7 @@ class Events(commands.Cog):
 
                 exceptions = [settings.DEBATE_CHANNEL, settings.RETARDVILLE_CHANNEL, settings.MEMES_CHANNEL, settings.ART_CHANNEL,
                 settings.FANART_CHANNEL, settings.SUGGESTIONS_CHANNEL, settings.EMOJI_SUGGESTIONS_CHANNEL, settings.PROPER_CHANNEL,
-                settings.WEEB_CHANNEL, settings.MAGAUS_CHANNEL]
+                settings.WEEB_CHANNEL, settings.MAGAUS_CHANNEL, settings.MUSIC_CHANNEL]
                 link_identifiers = ['www.', '.com', 'http://', 'https://']
 
                 # Check for server invites
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix = settings.COMMAND_PREFIX, description = setti
 bot.remove_command('help')
 
 for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
+    if filename.endswith('.py') and filename != 'music.py':
         bot.load_extension(f'cogs.{filename[:-3]}')
 
 if __name__ == '__main__':
