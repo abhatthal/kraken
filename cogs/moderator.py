da788eac0dba771107382e7b4cc0cbbfe6e0e68c
Akash Bhatthal <akashbhatthal@gmail.com>
2019-09-23 11:26:28 -0700

    added channel to unmute

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -420,6 +420,7 @@ class Moderator(commands.Cog):
     async def unmute(self, ctx, member : discord.Member, *, reason = 'Unspecified'):
         user_perms = await getListOfUserPerms(ctx)
         if 'manage_roles' in user_perms:
+            channel = self.bot.get_channel(settings.LOGGING_CHANNEL)
             mute = get(ctx.guild.roles, name = 'Muted')
             eObj = await embed(ctx, colour = 0x2D2D2D, author = f'{member} has been unmuted',
                 avatar = member.avatar_url, description = f'**Reason: **{reason}')
