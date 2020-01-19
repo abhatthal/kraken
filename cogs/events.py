f094047dbed967072c48ed0438fab47b98ecbe77
Akash Bhatthal <akashbhatthal@gmail.com>
2020-01-19 11:39:52 -0800

    remove fanart from events

--- cogs/events.py
+++ cogs/events.py
@@ -146,7 +146,7 @@ class Events(commands.Cog):
                                 break
 
                 exceptions = [settings.DEBATE_CHANNEL, settings.RETARDVILLE_CHANNEL, settings.MEMES_CHANNEL, settings.ART_CHANNEL,
-                settings.FANART_CHANNEL, settings.SUGGESTIONS_CHANNEL, settings.EMOJI_SUGGESTIONS_CHANNEL, settings.PROPER_CHANNEL,
+                 settings.SUGGESTIONS_CHANNEL, settings.EMOJI_SUGGESTIONS_CHANNEL, settings.PROPER_CHANNEL,
                 settings.WEEB_CHANNEL, settings.MAGAUS_CHANNEL, settings.MUSIC_CHANNEL, settings.GENERAL_VC_CHANNEL,
                 settings.COOKING_CHANNEL, settings.GAMING_CHANNEL]
                 link_identifiers = ['www.', '.com', 'http://', 'https://']
