6d07dab308a2561b86cb0b7a55f3e4bf2e16a035
Akash Bhatthal <abhattha@sfu.ca>
2020-04-21 17:51:36 -0700

    type edit_role => edit

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -544,7 +544,7 @@ class Moderator(commands.Cog):
                 # toggle sending messages for @everyone in all guilds
                 for guild in self.bot.guilds:
                     # await ctx.send(guild.name)
-                    await self.bot.edit_role(guild.default_role, send_messages=self.alarm_status)
+                    await self.bot.edit(guild.default_role, send_messages=self.alarm_status)
 
                 # flip alarm status
                 self.alarm_status = not self.alarm_status
