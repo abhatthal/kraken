af0d791e01fb1a5a1eaecad2eb2cff24c4c86b7f
Akash Bhatthal <akashbhatthal@gmail.com>
2020-06-01 21:44:36 -0700

    Description updated in settings

--- helper_files/settings.py
+++ helper_files/settings.py
@@ -6,7 +6,7 @@ logging.basicConfig(filename = 'bot.log', level = logging.INFO, format='%(asctim
 
 BOT_NAME = 'Kraken'
 BOT_AVATAR = 'https://raw.githubusercontent.com/abhatthal/Kraken/master/images/Kraken.png'
-DESCRIPTION = 'Part-time legendary sea monster, full-time Python 3 Discord bot.'
+DESCRIPTION = 'Part-time legendary sea monster, full-time Discord bot.'
 COMMAND_PREFIX = '.'
 
 DATABASE = 'kraken.db'
